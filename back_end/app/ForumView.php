fe2eb7335dab5391d4beb314c1317f88f2c10980
Raihan <raihannurr@gmail.com>
2017-05-03 14:44:45 +0700

    Fixed forum view

--- back_end/app/ForumView.php
+++ back_end/app/ForumView.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ForumView extends Model
 {
-    public static function newView(User $user, Thread $thread) {
+    public static function newView(User $user, Thread $viewed) {
+        $userId = $user ? $user->id : null;
+
+        if ($userId) {
+            $lastView = ForumView::where('user_id', $userId)->where('viewed_id', $viewed->id)->first();
+            if ($lastView) {
+                if ($lastView->created_at->diffInMinutes(\Carbon\Carbon::now()) <= 10)
+                    return;
+            }
+        }
         $view = new ForumView;
         $view->user_id = $user ? $user->id : null;
-        $view->viewed_id = $thread->id;
+        $view->viewed_id = $viewed->id;
         $view->save();
     }
 }
